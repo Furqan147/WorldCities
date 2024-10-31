f9ac295179e04dfe17a83a52aa4614f657a7092a
Furqan.Zafar <furqan_zfr@outlook.com>
2024-11-01 00:29:01 +0500

    Fix: Entities added for server project.

---
+++ WorldCities.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Server.Data.Models;
+
+namespace WorldCities.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext() : base()
+        {
+            
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries => Set<Country>();
+        public DbSet<City> Cities => Set<City>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
