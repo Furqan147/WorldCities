49cdfffc997ae375e6e9e0efacfdc9ab30f0475f
Furqan.Zafar <furqan_zfr@outlook.com>
2024-11-03 18:00:31 +0500

    Sorting feature implemented on both frontend and backend.

--- worldcities.client/src/app/angular-material.module.ts
+++ worldcities.client/src/app/angular-material.module.ts
@@ -4,19 +4,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 @NgModule({
  imports: [
  MatButtonModule,
  MatIconModule,
  MatToolbarModule,
- MatTableModule
+ MatTableModule,
+ MatSortModule
  ],
  exports: [
  MatButtonModule,
  MatIconModule,
  MatToolbarModule,
  MatTableModule,
- MatPaginatorModule
+ MatPaginatorModule,
+ MatSortModule
  ]
 })
 export class AngularMaterialModule { }
